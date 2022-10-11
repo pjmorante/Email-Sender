59a1fd1878aca7b7dce2434f8767bf112c4e918b
Pedro Morante <pedro.morante@outlook.com>
2022-10-10 21:28:44 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const btnEnviar = document.querySelector('#enviar');
+
+eventListeners();
+
+function eventListeners() {
+  document.addEventListener('DOMContentLoaded', iniciarApp);
+}
+
+function iniciarApp() {
+  btnEnviar.disabled = true;
+  btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
