ab87e7bddbcca841cb25d822c7b6fcd2fae8a03f
Pedro Morante <pedro.morante@outlook.com>
2022-10-10 21:59:54 -0500

    add fields validation

--- js/app.js
+++ js/app.js
@@ -1,12 +1,42 @@
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
+
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
 
 eventListeners();
 
 function eventListeners() {
   document.addEventListener('DOMContentLoaded', iniciarApp);
+
+  email.addEventListener('blur', validarFormulario);
+  asunto.addEventListener('blur', validarFormulario);
+  mensaje.addEventListener('blur', validarFormulario);
 }
 
 function iniciarApp() {
   btnEnviar.disabled = true;
   btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+function validarFormulario(e) {
+  if (e.target.value > 0) {
+    console.log('si hay algo');
+  } else {
+    e.target.classList.add('border', 'border-red-500');
+
+    mostrarError();
+  }
+}
+
+function mostrarError() {
+  const mensajeError = document.createElement('p');
+  mensajeError.textContent = 'Todos los campos son obligatorios';
+  mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+
+  const errores = document.querySelectorAll('.error');
+  if (errores.length === 0) {
+    formulario.appendChild(mensajeError);
+  }
 }
